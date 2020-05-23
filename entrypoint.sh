c449df54841bf4aac8652af3deb4ed47bd6539fb
wib-wub <prachayawut.s@gmail.com>
2020-05-24 04:22:15 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+ls
+
+cd functions; npm i
 
 if [ -z "${FIREBASE_TOKEN}" ]; then
     echo "FIREBASE_TOKEN is missing"
     exit 1
 fi
 
-cd functions; npm i
 
 firebase deploy \
     --token ${FIREBASE_TOKEN} \
